d949aed7f1ff0fadaaa2d59f055928303cf3680f
Anas Na <74872757+itsanasna@users.noreply.github.com>
2022-03-31 20:52:58 -0400

    implemented CRUD for rooms

--- back-end/controllers/meetingRoomsController.js
+++ back-end/controllers/meetingRoomsController.js
@@ -2,11 +2,30 @@ const express = require("express");
 
 const meetingRooms = express.Router();
 
-const { getAllRooms } = require("../queries/meetingRooms.js");
+const { getAllRooms, getRoom, createRoom, updateRoom, deleteRoom } = require("../queries/meetingRooms.js");
 
 meetingRooms.get("/", async (req, res) => {
   // const meetingRooms = await getAllRooms();
   res.json(await getAllRooms());
+});
+
+meetingRooms.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    res.json(await getRoom(id));
+});
+
+meetingRooms.post("/", async (req, res) => {
+  res.json(await createRoom(req.body));
+}); 
+
+meetingRooms.put("/:id", async (req, res) => {
+  const { id } = req.params
+  res.json(await updateRoom(id, req.body))
+});
+
+meetingRooms.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  res.json(await deleteRoom(id))
 });
 
 module.exports = meetingRooms;
--- back-end/queries/meetingRooms.js
+++ back-end/queries/meetingRooms.js
@@ -10,6 +10,54 @@ const getAllRooms = async () => {
   }
 };
 
+const getRoom = async (id) => {
+    try {
+        const room = await db.one(`SELECT * FROM meetingrooms WHERE id = $1`, id);
+        return room;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const createRoom = async (newRoom) => {
+    const { room_name, room_capacity, room_floor } = newRoom;
+    try {
+        const createdRoom = await db.one(
+            `INSERT INTO meetingrooms(room_name, room_capacity, room_floor) VALUES($1, $2, $3) RETURNING *`,
+            [room_name, room_capacity, room_floor]
+        );
+        return createdRoom;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const updateRoom = async (id, room) => {
+  try {
+      const { room_name, room_capacity, room_floor} = room;
+      const query = "UPDATE meetingrooms SET room_name = $1, room_capacity = $2, room_floor = $3 WHERE id = $4 RETURNING *";
+      const updatedRoom = await db.one(query, [room_name, room_capacity, room_floor, id]);
+      return updatedRoom;
+  } catch (error) {
+      return error;
+  }
+};
+
+const deleteRoom = async (id) => {
+  try {
+      const query = "DELETE FROM meetingrooms WHERE id = $1 RETURNING *";
+      const deletedRoom = await db.one(query, id);
+      return deletedRoom;
+  } catch (error) {
+      console.log(error);
+  }
+};
+
+
 module.exports = {
-  getAllRooms
+  getAllRooms,
+  getRoom,
+  createRoom,
+  updateRoom,
+  deleteRoom
 };
